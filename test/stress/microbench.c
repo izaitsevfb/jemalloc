5a48b0c9e9104e97a7b45b13acd1f3d23b92e62e
Ivan Zaitsev <ivanzaitsev@fb.com>
2022-07-20 16:32:18 -0700

    simple malloc(1) + free() tight loop benchmark

--- test/stress/microbench.c
+++ test/stress/microbench.c
@@ -1,6 +1,30 @@
 #include "test/jemalloc_test.h"
 #include "test/bench.h"
 
+static inline void
+time_single_func(
+    uint64_t nwarmup, uint64_t niter, const char *name_a, void(*func_a)) {
+	timedelta_t timer_a;
+	void *p;
+
+	p = mallocx(1, 0);
+	if (p == NULL) {
+		test_fail("Unexpected mallocx() failure");
+		return;
+	}
+
+	time_func(&timer_a, nwarmup, niter, func_a);
+
+	uint64_t usec_a = timer_usec(&timer_a);
+	char buf_a[FMT_NSECS_BUF_SIZE];
+	fmt_nsecs(usec_a, niter, buf_a);
+
+	malloc_printf("%"FMTu64" iterations, %s=%"FMTu64"us (%s ns/iter), ",
+	    niter, name_a, usec_a, buf_a);
+
+	dallocx(p, 0);
+}
+
 static void
 malloc_free(void) {
 	/* The compiler can optimize away free(malloc(1))! */
@@ -12,115 +36,13 @@ malloc_free(void) {
 	free(p);
 }
 
-static void
-mallocx_free(void) {
-	void *p = mallocx(1, 0);
-	if (p == NULL) {
-		test_fail("Unexpected mallocx() failure");
-		return;
-	}
-	free(p);
-}
-
-TEST_BEGIN(test_malloc_vs_mallocx) {
-	compare_funcs(10*1000*1000, 100*1000*1000, "malloc",
-	    malloc_free, "mallocx", mallocx_free);
-}
-TEST_END
-
-static void
-malloc_dallocx(void) {
-	void *p = malloc(1);
-	if (p == NULL) {
-		test_fail("Unexpected malloc() failure");
-		return;
-	}
-	dallocx(p, 0);
-}
-
-static void
-malloc_sdallocx(void) {
-	void *p = malloc(1);
-	if (p == NULL) {
-		test_fail("Unexpected malloc() failure");
-		return;
-	}
-	sdallocx(p, 1, 0);
-}
-
-TEST_BEGIN(test_free_vs_dallocx) {
-	compare_funcs(10*1000*1000, 100*1000*1000, "free", malloc_free,
-	    "dallocx", malloc_dallocx);
-}
-TEST_END
-
-TEST_BEGIN(test_dallocx_vs_sdallocx) {
-	compare_funcs(10*1000*1000, 100*1000*1000, "dallocx", malloc_dallocx,
-	    "sdallocx", malloc_sdallocx);
-}
-TEST_END
-
-static void
-malloc_mus_free(void) {
-	void *p;
-
-	p = malloc(1);
-	if (p == NULL) {
-		test_fail("Unexpected malloc() failure");
-		return;
-	}
-	TEST_MALLOC_SIZE(p);
-	free(p);
-}
-
-static void
-malloc_sallocx_free(void) {
-	void *p;
-
-	p = malloc(1);
-	if (p == NULL) {
-		test_fail("Unexpected malloc() failure");
-		return;
-	}
-	if (sallocx(p, 0) < 1) {
-		test_fail("Unexpected sallocx() failure");
-	}
-	free(p);
-}
-
-TEST_BEGIN(test_mus_vs_sallocx) {
-	compare_funcs(10*1000*1000, 100*1000*1000, "malloc_usable_size",
-	    malloc_mus_free, "sallocx", malloc_sallocx_free);
-}
-TEST_END
-
-static void
-malloc_nallocx_free(void) {
-	void *p;
-
-	p = malloc(1);
-	if (p == NULL) {
-		test_fail("Unexpected malloc() failure");
-		return;
-	}
-	if (nallocx(1, 0) < 1) {
-		test_fail("Unexpected nallocx() failure");
-	}
-	free(p);
-}
-
-TEST_BEGIN(test_sallocx_vs_nallocx) {
-	compare_funcs(10*1000*1000, 100*1000*1000, "sallocx",
-	    malloc_sallocx_free, "nallocx", malloc_nallocx_free);
+TEST_BEGIN(bench_malloc_free) {
+	time_single_func(
+	    5 * 1000 * 1000, 10 * 1000 * 1000, "malloc_free", malloc_free);
 }
 TEST_END
 
 int
 main(void) {
-	return test_no_reentrancy(
-	    test_malloc_vs_mallocx,
-	    test_free_vs_dallocx,
-	    test_dallocx_vs_sdallocx,
-	    test_mus_vs_sallocx,
-	    test_sallocx_vs_nallocx);
+	return test_no_reentrancy(bench_malloc_free);
 }
